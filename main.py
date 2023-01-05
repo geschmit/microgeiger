151f55c82c02d2c0902c7f4f8a6f9582606b5345
Gabe Schmit <119266089+geschmit@users.noreply.github.com>
2023-01-05 05:30:43 +0000

    do the hokey pokey

--- main.py
+++ main.py
@@ -1,7 +1,67 @@
 # (c) 2022 geschmit
 
+# geiger module
 from geiger import Geiger
-from board import I2C, SPI
 
-geig = Geiger(i2c=I2C())
-print("Geiger firm ver: %s", str(geig.firmVer))
+# display
+from displayio import FourWire
+from adafruit_ili9341 import ILI9341
+
+# neopixel
+from neopixel import NeoPixel
+from rainbowio import colorwheel
+
+# multiple
+from digitalio import DigitalInOut, Direction, Pull
+from busio import SPI, I2C
+from time import sleep
+from board import (
+    # Display SPI interface
+    SCK,
+    MOSI,
+    MISO,
+
+    # Additional display pins
+    D10, # Command
+    D9,  # CS
+    D6,  # Reset
+
+    # I2C interface
+    SCL,
+    SDA,
+
+    # Leds
+    RED_LED,
+    BLUE_LED,
+    NEOPIXEL
+)
+
+# Initialize the stuff
+pix = NeoPixel(NEOPIXEL,1,brightness=0.3,auto_write=False)
+bLed = DigitalInOut(BLUE_LED)
+rLed = DigitalInOut(RED_LED)
+
+bus = FourWire(
+    SPI(clock=SCK,MOSI=MOSI,MISO=MISO), command=D10, chip_select=D9, reset=D6
+)
+
+disp = ILI9341(bus, width=320, height=240)
+geig = Geiger(i2c=I2C(scl=SCL, sda=SDA))
+
+# the stuff we work with
+neoRgb = False # currently cycling neopixel rgb? (gaming geiger)
+
+# the stuff we shouldn't change
+vers = 1.0
+
+print("HELLO- MicroGeiger")
+print("(c) 2022 geschmit")
+print("Funded by Hack Club!")
+print("mGeig ver. %s", str(vers))
+print("Awaiting geiger response...")
+print("Geiger ver: %s", str(geig.firmVer))
+
+
+# the engine belt
+while True:
+    pass
